abe50335d07b3feb6d27cab7db135ddb8ec83de4
Rebecka Kinn <rebeckaki@getacademy.no>
2026-05-21 14:45:05 +0200

    Add project files.

---
+++ Calculation.cs
@@ -0,0 +1,18 @@
+﻿namespace BrokenCalculator
+{
+    class Calculation
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+        public string Operation { get; set; }
+        public int Result { get; set; }
+
+        public Calculation(int number1, int number2, string operation, int result)
+        {
+            Number1 = number1;
+            Number2 = number2;
+            Operation = operation;
+            Result = result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace BrokenCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Menu menu = new Menu();
+
+            bool running = true;
+
+            while (running)
+            {
+                menu.Show();
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        calculator.AddNumbers();
+                        break;
+
+                    case "2":
+                        calculator.SubtractNumbers();
+                        break;
+
+                    case "3":
+                        calculator.ShowHistory();
+                        break;
+
+                    case "4":
+                        calculator.ShowHighestResult();
+                        break;
+
+                    case "5":
+                        calculator.ShowAverageResult();
+                        break;
+
+                    case "6":
+                        running = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+            }
+        }
+    }
+}
